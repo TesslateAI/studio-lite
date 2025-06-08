9de7be04b93d7373e0eb8c26d7a43add893da23f
Manav M <kookygavinquack@gmail.com>
2025-06-08 02:19:48 -0400

    fixed db errors

--- lib/db/migrations/0002_nebulous_pete_wisdom.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE "stripe" ALTER COLUMN "plan_name" SET DEFAULT 'Free';--> statement-breakpoint
-ALTER TABLE "stripe" ALTER COLUMN "plan_name" SET NOT NULL;--> statement-breakpoint
-ALTER TABLE "stripe" ALTER COLUMN "subscription_status" SET DEFAULT 'inactive';--> statement-breakpoint
-ALTER TABLE "stripe" ALTER COLUMN "subscription_status" SET NOT NULL;
--- lib/db/migrations/0004_lame_reavers.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "users" ADD COLUMN "is_guest" boolean DEFAULT false NOT NULL;--> statement-breakpoint
-ALTER TABLE "users" ALTER COLUMN "email" DROP NOT NULL;--> statement-breakpoint
-ALTER TABLE "users" ALTER COLUMN "password_hash" DROP NOT NULL;
